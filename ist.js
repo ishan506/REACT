bf84356be20da69435631a72eb8d4f8fc7902ea6
Ishan <ishxn36@gmail.com>
2026-06-27 20:01:44 +0530

    project.js

---
+++ ist.js
@@ -0,0 +1,49 @@
+/*const heading =document.createElement("h1");
+ heading.innerHTML="nam";
+ const root=document.getElementById("root");
+ root.appendChild(heading);*/
+import React from "react"
+import ReactDOM from "react-dom/Client"
+/*const heading = React.createElement("h1", { id: "title",className:"p" }, "nam");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+const head = React.createElement(
+  "div",
+  {style: {
+            color: "red",
+            backgroundColor: "yellow"
+        }},
+  [
+    React.createElement("h1", {key:1}, "first"),
+    React.createElement("h2", {key:2}, "second"),
+  ],
+  "name",
+ 
+);
+
+const r = ReactDOM.createRoot(document.getElementById("r"));
+r.render(head);*/
+/*const parent = React.createElement(
+    "div",
+    {},
+    React.createElement(
+        "h1",
+        {},
+        "Hello"
+    )
+);
+const H=
+  (<div><h1>hii</h1></div>)
+;
+ const Heading =()=>{ (
+  <div><h1>hiii
+    </h1>{H}</div>);};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+ root.render(<Heading/>);*/
+
+
+const Middle=<div><h2>h</h2></div>;
+const T=(<div className="ti"><h1>hii{Middle}</h1><h2>by</h2></div>);
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(T);
--- react.js
+++ react.js
@@ -1,49 +1,21 @@
-/*const heading =document.createElement("h1");
- heading.innerHTML="nam";
- const root=document.getElementById("root");
- root.appendChild(heading);*/
-import React from "react"
+ import React from "react"
 import ReactDOM from "react-dom/Client"
-/*const heading = React.createElement("h1", { id: "title",className:"p" }, "nam");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
-
-const head = React.createElement(
-  "div",
-  {style: {
-            color: "red",
-            backgroundColor: "yellow"
-        }},
-  [
-    React.createElement("h1", {key:1}, "first"),
-    React.createElement("h2", {key:2}, "second"),
-  ],
-  "name",
+import "./m.css"
  
-);
-
-const r = ReactDOM.createRoot(document.getElementById("r"));
-r.render(head);*/
-/*const parent = React.createElement(
-    "div",
-    {},
-    React.createElement(
-        "h1",
-        {},
-        "Hello"
-    )
-);
-const H=
-  (<div><h1>hii</h1></div>)
-;
- const Heading =()=>{ (
-  <div><h1>hiii
-    </h1>{H}</div>);};
-const root = ReactDOM.createRoot(document.getElementById("root"));
- root.render(<Heading/>);*/
-
-
-const Middle=<div><h2>h</h2></div>;
-const T=(<div className="ti"><h1>hii{Middle}</h1><h2>by</h2></div>);
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(T);
+
+ 
+const Fun=()=>(
+    
+     
+        <div>
+<img src= "1107.jpeg"></img><div className="header">
+    <div className="logo"><div>SARPANCH KA DABBA</div></div>
+<ul><li>home</li><li>menu</li></ul></div>
+    </div>
+     );
+     const body =()=>{
+        
+     }
+    
+root.render(<Fun />);
